65d33857a272d01aef8135f6a86fdafd14ab00de
Michael Lawrence <michafla@gene.com>
2016-03-27 04:16:37 -0700

    R-side raggedListToDF support for NULL elements

--- R/utils.R
+++ R/utils.R
@@ -2,11 +2,18 @@
 ### Low-level utilities
 ### -------------------------------------------------------------------------
 
-raggedListToDF <- function(x, ...) {
+raggedListToDF <- function(x, keepAlwaysNULL = TRUE, ...) {
   nms <- unlist(lapply(x, names))
   uniq.nms <- unique(nms)
   ind <- match(nms, uniq.nms)
-  as.data.frame(.Call(R_raggedListToDF, x, uniq.nms, ind), ...)
+  cols <- .Call(R_raggedListToDF, x, uniq.nms, ind)
+  nulls <- vapply(cols, is.null, logical(1L))
+  if (keepAlwaysNULL) {
+      cols[nulls] <- list(rep(NA, length(x)))
+  } else {
+      cols[nulls] <- NULL
+  }
+  as.data.frame(cols, ...)
 }
 
 setMethod("unstrsplit", "AsIs", function(x, sep = "") {
